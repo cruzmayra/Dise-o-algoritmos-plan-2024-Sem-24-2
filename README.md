3e1eca7a18702ef3d46447e5211daaef76eba561
cruzmayra <velazquez.leonora@gmail.com>
2024-05-19 14:32:08 -0600

    Algoritmo para ordenar números ingresados por el usuario

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# Diseño de algoritmos. Plan-2024. Semestre 24-2
-Repositorio que recopila las actividades generadas en el semestre 24-2 de la asignatura de Diseño de Algoritmos
+# Unidad 6. Actividad 3.
+A partir del estudio de la unidad, elabora un programa que cumpla el siguiente objetivo:
+
+El programa deberá solicitar al usuario 3 números, los cuales deberán ser mayores de 3 y menores de 15.
+
+El programa deberá regresar los números ordenados de mayor a menor e indicar un error si el usuario ingresa números fuera de los parámetros establecidos.
+
+
+Autora
+===============
+
+__[Mayra Cruz](https://github.com/cruzmayra)__
---
+++ main.c
@@ -0,0 +1,40 @@
+#include <stdbool.h>
+#include <stdio.h>
+
+bool numberValidation(int number) {
+    return number >= 3 && number <= 15;
+};
+
+int main() {
+    int a, b, c;
+
+    printf("Introduce tres numeros separados por espacios, por favor:\n");
+    scanf("%d%d%d", &a, &b, &c);
+
+    if(numberValidation(a) && numberValidation(b) && numberValidation(c)) {
+        int numbersArray[] = {a, b, c};
+        int limit = sizeof(numbersArray) / sizeof(numbersArray[0]);
+        int i, j;
+        for (i = 0; i < limit - 1; i++) {
+            for (j = 0; j < (limit - i) - 1; j++) {
+                if (numbersArray[j] > numbersArray[j + 1]) {
+                    int temp = numbersArray[j];
+                    numbersArray[j] = numbersArray[j + 1];
+                    numbersArray[j + 1] = temp;
+                }
+            }
+        }
+
+        printf("\nEstos son los numeros ordenados! \n");
+
+        for (int i = 0; i < limit; i++) {  
+            printf("%d ", numbersArray[i]);  
+        }
+
+    } else {
+        printf("\nPor favor, introduce numeros mayores a 3 y menores a 15.\n");
+    }
+
+
+    return 0;
+}
